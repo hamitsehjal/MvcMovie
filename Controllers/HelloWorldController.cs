8e524214123efd16621f3d032ace761e547ba0c7
Hamit Sehjal <hamitsehjal26@gmail.com>
2023-05-19 12:15:10 -0400

    Add project files.

---
+++ Controllers/HelloWorldController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+
+        // GET: /HelloWorld
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: /HelloWorld/Welcome/
+        public IActionResult Welcome(string name,int count=1) {
+            ViewData["greeting"] = "Hi " + name + " !! How are you doing??";
+            ViewData["count"] = count;
+            return View();
+
+        }
+    }
+}
